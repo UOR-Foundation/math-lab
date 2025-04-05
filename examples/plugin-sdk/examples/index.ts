9bd7379d9787f219bfb12a53217c04de054d33eb
Alex Flom <alexander.flom@gmail.com>
2025-04-05 20:31:38 +0000

    Move examples to separate directory and exclude from build
    - Creates a separate examples directory outside of the main source code
    - Updates tsconfig.json to explicitly exclude examples from type checking
    - Adds a README explaining the separation

---
+++ examples/plugin-sdk/examples/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Plugin SDK Examples
+ * 
+ * Example plugin implementations using the SDK
+ */
+
+export { BasicExamplePlugin } from './basic-plugin';
+export { NumberTheoryPlugin } from './number-theory-plugin';
